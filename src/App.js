7f4b26cfad97ed9c662586610e23a8d3aed6ea52
Peter <peterdotliu@gmail.com>
2020-09-24 21:58:46 -0400

    Implement state for controlled forms

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Timer from './components/Timer'
+import TimerForm from './components/TimerForm'
+import Refresher from './components/Refresher'
 import { Container, Row, Col } from 'react-bootstrap'
 
 const App = () => {
-  const [countdown, setCountdown] = useState(10000)
+  const [baseCd, setBaseCd] = useState(15000)
+  const [baseRf, setBaseRf] = useState()
+  const [countdown, setCountdown] = useState(baseCd)
+  const [refresh, setRefresh] = useState(0)
   const [notifPerm, setPerm] = useState(Notification.permission)
+
+  // if (!countdown) {
+  //   const interval = setInterval(() => setRefresh(refresh - 1000), 1000)
+  //   return () => clearInterval(interval)
+  // }
 
   if (notifPerm !== 'granted') {
     Notification.requestPermission().then(result => {
@@ -12,19 +22,27 @@ const App = () => {
     })
   }
 
-  if (!countdown) {
-    const notif = new Notification(
-      'Refresh your mind! Stand up, stretch, drink some water. You deserve it!'
-    )
-  }
-
   return (
     <div id='app'>
       <Container>
         {notifPerm === 'granted' ? (
-          <Timer countdown={countdown} setCountdown={setCountdown} />
+          <Timer
+            countdown={countdown}
+            setCountdown={setCountdown}
+            cd={baseCd}
+            setRefresh={setRefresh}
+          />
         ) : (
           <h2>This page requires Notification permissions to run</h2>
+        )}
+        {refresh ? (
+          <Refresher
+            refresh={refresh}
+            setRefresh={setRefresh}
+            countdown={countdown}
+          />
+        ) : (
+          ''
         )}
       </Container>
     </div>
