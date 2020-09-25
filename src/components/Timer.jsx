841fd0477d6bbab10866689efbc95ce2c92d552c
Peter <peterdotliu@gmail.com>
2020-09-24 22:02:46 -0400

    Seperate countdown logic from app.js

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect } from 'react'
 
-const Timer = ({ countdown, setCountdown }) => {
+const Timer = ({ countdown, setCountdown, setRefresh, cd }) => {
   useEffect(() => {
     if (countdown) {
       const interval = setInterval(() => setCountdown(countdown - 1000), 1000)
       return () => clearInterval(interval)
     }
   })
+  if (!countdown) {
+    const notif = new Notification(
+      `It's been ${cd / 60000} minutes. Take a quick break!`,
+      { body: 'Stand up, stretch, grab a drink! You deserve it! <3' }
+    )
+    setRefresh(15000)
+  }
 
   const ss = Math.floor((countdown / 1000) % 60).toLocaleString(undefined, {
     minimumIntegerDigits: 2,
