738dba07173e09efced6234208212708ea6796b6
Peter <peterdotliu@gmail.com>
2020-10-08 22:14:11 -0400

    Rework effect hook logic for sending Notification and setting refresh timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,20 +1,6 @@
 import React, { useEffect } from 'react'
 
-const Timer = ({ countdown, setCountdown, setRefresh, cd }) => {
-  useEffect(() => {
-    if (countdown) {
-      const interval = setInterval(() => setCountdown(countdown - 1000), 1000)
-      return () => clearInterval(interval)
-    }
-  })
-  if (!countdown) {
-    const notif = new Notification(
-      `It's been ${cd / 60000} minutes. Take a quick break!`,
-      { body: 'Stand up, stretch, grab a drink! You deserve it! <3' }
-    )
-    setRefresh(15000)
-  }
-
+const Timer = ({ countdown, setCountdown, baseCd, baseRf, setRefresh }) => {
   const ss = Math.floor((countdown / 1000) % 60).toLocaleString(undefined, {
     minimumIntegerDigits: 2,
   })
@@ -30,7 +16,22 @@ const Timer = ({ countdown, setCountdown, setRefresh, cd }) => {
       minimumIntegerDigits: 2,
     }
   )
+  useEffect(() => {
+    if (countdown) {
+      const interval = setInterval(() => setCountdown(countdown - 1000), 1000)
+      return () => clearInterval(interval)
+    }
+  })
 
+  useEffect(() => {
+    if (!countdown) {
+      const notif = new Notification(
+        `It's been ${baseCd / 60000} minutes. Take a quick break!`,
+        { body: 'Stand up, stretch, grab a drink! You deserve it! <3' }
+      )
+      setRefresh(baseRf)
+    }
+  }, [countdown])
   return (
     <div>
       {hh}:{mm}:{ss}
